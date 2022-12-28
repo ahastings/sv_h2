f2a825502ec6c95abcb5dda9327e77790df39e15
AHastings <armanhastings@gmail.com>
2022-12-27 22:55:47 -0800

    v2 130m

--- learnply_2021/learnply/Polyline.cpp
+++ learnply_2021/learnply/Polyline.cpp
@@ -73,29 +73,151 @@ void display_polyline(std::vector<POLYLINE>& polylines) {
 }
 
 
-void marchingSqure(std::list<POLYLINE>& edges,
+/*void marchingSqure(std::list<POLYLINE>& edges,
 	const Polyhedron& poly,
 	const double& thres) {
 
+}*/
+
+Vertex linearInterpolateByScalar(const Vertex& v0, const Vertex& v1, const double& thres) {
+	double f0_f1 = v0.scalar - v1.scalar;
+	Vertex r(0.0, 0.0, 0.0);
+	if (std::abs(f0_f1) < 1.0e-5) {
+		r.x = (v0.x + v1.x) / 2;
+		r.y = (v0.y + v1.y) / 2;
+		r.z = (v0.z + v1.z) / 2;
+	}
+	else {
+		double t = std::abs((v0.scalar - thres) / ((v0.scalar - thres) - (v1.scalar - thres)));
+		r.x = v0.x + t * (v1.x - v0.x);
+		r.y = v0.y + t * (v1.y - v0.y);
+		r.z = v0.z + t * (v1.z - v0.z);
+	}
+	return r;
 }
 
 
 void lookUpTable(
-	std::vector<Vertex>& r,
+	std::vector<Vertex> &r,
 	const Vertex& v0,
 	const Vertex& v1,
 	const Vertex& v2,
 	const Vertex& v3,
-	const double& thres){
+	const double& thres) {
 
+	r.reserve(2);
+	int id = 0;
+	if (v0.scalar <= thres + EPSILON) {
+		id += 1;
+	}
+	if (v1.scalar <= thres + EPSILON) {
+		id += 2;
+	}
+	if (v2.scalar <= thres + EPSILON) {
+		id += 4;
+	}
+	if (v3.scalar <= thres + EPSILON) {
+		id += 8;
+	}
+	double center = 0;
+	switch (id)
+	{
+	case 0:
+		break;
+	case 1:
+		r.push_back(linearInterpolateByScalar(v0, v1, thres));
+		r.push_back(linearInterpolateByScalar(v0, v3, thres));
+		break;
+	case 2:
+		r.push_back(linearInterpolateByScalar(v0, v1, thres));
+		r.push_back(linearInterpolateByScalar(v1, v2, thres));
+		break;
+	case 3:
+		r.push_back(linearInterpolateByScalar(v1, v2, thres));
+		r.push_back(linearInterpolateByScalar(v0, v3, thres));
+		break;
+	case 4:
+		r.push_back(linearInterpolateByScalar(v1, v2, thres));
+		r.push_back(linearInterpolateByScalar(v2, v3, thres));
+		break;
+	case 5:
+		center = v0.scalar, v1.scalar, v2.scalar, v3.scalar;
+		center /= 4;
+		if (center <= thres) {
+			r.push_back(linearInterpolateByScalar(v0, v1, thres));
+			r.push_back(linearInterpolateByScalar(v1, v2, thres));
+			r.push_back(linearInterpolateByScalar(v2, v3, thres));
+			r.push_back(linearInterpolateByScalar(v0, v3, thres));
+		}
+		else {
+			r.push_back(linearInterpolateByScalar(v0, v1, thres));
+			r.push_back(linearInterpolateByScalar(v0, v3, thres));
+			r.push_back(linearInterpolateByScalar(v1, v2, thres));
+			r.push_back(linearInterpolateByScalar(v2, v3, thres));
+		}
+		break;
+	case 6://same as 9
+		r.push_back(linearInterpolateByScalar(v0, v1, thres));
+		r.push_back(linearInterpolateByScalar(v2, v3, thres));
+		break;
+	case 7:
+		r.push_back(linearInterpolateByScalar(v2, v3, thres));
+		r.push_back(linearInterpolateByScalar(v0, v3, thres));
+		break;
+	case 8:
+		r.push_back(linearInterpolateByScalar(v2, v3, thres));
+		r.push_back(linearInterpolateByScalar(v0, v3, thres));
+		break;
+	case 9: //same as 6
+		r.push_back(linearInterpolateByScalar(v0, v1, thres));
+		r.push_back(linearInterpolateByScalar(v2, v3, thres));
+		break;
+	case 10:
+		center = v0.scalar + v1.scalar + v2.scalar + v3.scalar;
+		center /= 4;
+		if (center <= thres) {
+			r.push_back(linearInterpolateByScalar(v0, v1, thres));
+			r.push_back(linearInterpolateByScalar(v0, v3, thres));
+			r.push_back(linearInterpolateByScalar(v1, v2, thres));
+			r.push_back(linearInterpolateByScalar(v2, v3, thres));
+		}
+		else {
+			r.push_back(linearInterpolateByScalar(v0, v1, thres));
+			r.push_back(linearInterpolateByScalar(v1, v2, thres));
+			r.push_back(linearInterpolateByScalar(v2, v3, thres));
+			r.push_back(linearInterpolateByScalar(v0, v3, thres));
+		}break;
+	case 11:
+		// v1 - v2 //v2- v3
+		r.push_back(linearInterpolateByScalar(v1, v2, thres));
+		r.push_back(linearInterpolateByScalar(v2, v3, thres));
+	break; case 12:
+		// v2 v2 //ve- v3
+		r.push_back(linearInterpolateByScalar(v1, v2, thres));
+		r.push_back(linearInterpolateByScalar(v0, v3, thres));
+	break; case 13:
+		// v0 - vi //v1- v2
+		r.push_back(linearInterpolateByScalar(v0, v1, thres));
+		r.push_back(linearInterpolateByScalar(v1, v2, thres));
+		break;
+	case 14:
+		// v0 - v1 //v0- v3
+		r.push_back(linearInterpolateByScalar(v0, v1, thres));
+		r.push_back(linearInterpolateByScalar(v0, v3, thres));
+		break;
+	case 15:
+		break;
+		
+
+	}
 }
 
-
-void marchingSquare(
-	std::list<POLYLINE>& edges, const Polyhedron& poly,
+void marchingSqure(
+	std::list<POLYLINE>& edges,
+	const Polyhedron& poly,
 	const double& thres) {
 	for (int i = 0; i < poly.nquads; i++) {
-		std::vector<Vertex> r;
+		std::vector<Vertex>r;
 		lookUpTable(r,
 			*poly.qlist[i]->verts[0],
 			*poly.qlist[i]->verts[1],
@@ -113,25 +235,33 @@ void marchingSquare(
 					r[r_i * 2 + 1].x,
 					r[r_i * 2 + 1].y,
 					r[r_i * 2 + 1].z);
-				/*if((ve - v1).length() < EPSILON)
-				------------
-				line.m_vertices.push_back(v®);
-				else {
-				line.m_vertices.push_back(ve);
-				line.m_vertices.push_back(v1);
-				}*/
 				line.m_vertices.push_back(v0);
 				line.m_vertices.push_back(v1);
 				edges.push_back(line);
 			}
 		}
-
 	}
 }
-
 
 void makePolylineFromEdges(
 	std::vector<POLYLINE>& polylines,
 	const std::list <POLYLINE>& edges){
-
+	polylines.reserve(edges.size());
+	std::list<POLYLINE> edges_temp(edges);
+	while (edges_temp.size() > 0) {
+		polylines.push_back(edges_temp.front());
+		edges_temp.erase(edges_temp.begin());
+		int init_size = 0;
+		while (init_size != edges_temp.size()) {
+			init_size = edges_temp.size();
+			for (auto i = edges_temp.begin(); i != edges_temp.end();) {
+				if (polylines.back().isNeighbor(*i)) {
+					polylines.back().merge(*i);
+					i = edges_temp.erase(i);
+				}
+				else
+					i++;
+			}
+		}
+	}
 }
